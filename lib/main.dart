965469e7de9ae55094cc323b643ff2be540a5920
NasciGN <gnascimento.job@gmail.com>
2023-06-04 15:54:05 -0400

    1.2 - Added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:livraria_da_domitilda/views/login_page.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     home: LoginPage(),
   ));
